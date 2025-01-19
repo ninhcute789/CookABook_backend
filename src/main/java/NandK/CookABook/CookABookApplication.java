c6d56be48f1789e3a7861c2cf8bd09f6b7597846
Hai núa <haininh172004@gmail.com>
2025-01-19 16:34:07 +0700

    update project

---
+++ src/main/java/NandK/CookABook/CookABookApplication.java
@@ -0,0 +1,13 @@
+package NandK.CookABook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookABookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookABookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NandK/CookABook/CookABookApplicationTests.java
@@ -0,0 +1,13 @@
+package NandK.CookABook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookABookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
