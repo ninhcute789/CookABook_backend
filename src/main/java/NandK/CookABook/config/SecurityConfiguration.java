6be39d235554a530b4e7c471bbd246b5b107292d
Hai núa <haininh172004@gmail.com>
2025-03-14 01:50:21 +0700

    update api for FE

--- src/main/java/NandK/CookABook/config/SecurityConfiguration.java
+++ src/main/java/NandK/CookABook/config/SecurityConfiguration.java
@@ -44,7 +44,7 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults()) // cho phep cors
                 .authorizeHttpRequests(
                         authz -> authz.requestMatchers("/", "/api/v1/auth/login",
-                                "/api/v1/auth/refresh").permitAll()
+                                "/api/v1/auth/refresh", "/api/v1/users/register", "/api/v1/articles/all").permitAll()
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
